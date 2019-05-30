3228e36918b0a8fe6db774eaeadab3622524474e
Ronald Kisor <rwkisor@gmail.com>
2019-05-30 17:43:19 -0500

    Completed
    todo: comment code

--- weatherAnalysis.py
+++ weatherAnalysis.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 
 cityData = pd.read_csv ('cityData.csv')
 globalData = pd.read_csv ('globalData.csv')
-cityData = cityData.drop (['city', 'country'], axis=1)
 cityData['city_temp'] = cityData['avg_temp'].rolling(7).mean ()
 globalData['global_temp'] = globalData['avg_temp'].rolling(7).mean ()
+
 ax = plt.gca ()
-cityData.plot (kind='line',x='year',y='city_temp',color='red', ax=ax)
-globalData.plot (kind='line',x='year',y='global_temp',color='blue', ax=ax)
-plt.show ()
+cityPlot = cityData.plot (kind='line',x='year',y='city_temp',color='red',label=str (cityData['city'][1]), ax=ax)
+globalPlot = globalData.plot (kind='line',x='year',y='global_temp', color='blue',label='Global', ax=ax)
+plt.suptitle ('Average Yearly Temperature Comparison')
+plt.ylabel ('Temperature (Celcius)')
+plt.xlabel ('Year')
+plt.savefig ('TempComp.png')
